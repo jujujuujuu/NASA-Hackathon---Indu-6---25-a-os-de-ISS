575c45f36ae3808ce52d335dbc0e2fc7342bd89f
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2025-10-04 16:45:52 -0300

    tierra sexi

--- nasa---hackathon---simulación-de-ingravidez-y-visualización-de-la-tierra/scripts/world.gd
+++ nasa---hackathon---simulación-de-ingravidez-y-visualización-de-la-tierra/scripts/world.gd
@@ -10,8 +10,8 @@ func _ready():
 		tween = create_tween()
 		tween.set_trans(Tween.TRANS_LINEAR)
 		
-		tween.tween_property($Earth, "rotation:y", deg_to_rad(359), 10)
-		await get_tree().create_timer(10).timeout
+		tween.tween_property($Earth, "rotation:y", deg_to_rad(359), 86400)
+		await get_tree().create_timer(86400).timeout
 
 func _process(delta: float) -> void:
 	#var angular_speed = earth_rot_speed/Global.earth_radius
